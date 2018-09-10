fc00fa70b5bb08a2f32015090d41ff7827b2fca9
Vitaly Kiselev <st411ar@ya.ru>
2018-09-10 13:55:29 +0300

    updated versions, removed vulnerabilities

--- src/app/paperlib.service.ts
+++ src/app/paperlib.service.ts
@@ -1,28 +1,31 @@
 import {Http} from '@angular/http';
 import {Injectable} from '@angular/core';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+
 
 @Injectable()
 export class PaperlibService {
   constructor(private http: Http) {}
 
   getBooks() {
+    this.http.get('').pipe(map(response => response.json()));
+
     return this.http.get('http://localhost:4999/paperlib/books')
-      .map(response => response.json());
+      .pipe(map(response => response.json()));
   }
 
   getBook(id) {
     return this.http.get('http://localhost:4999/paperlib/books/' + id)
-      .map(response => response.json());
+      .pipe(map(response => response.json()));
   }
 
   getUsers() {
     return this.http.get('http://localhost:4999/paperlib/users')
-      .map(response => response.json());
+      .pipe(map(response => response.json()));
   }
 
   getUser(id) {
     return this.http.get('http://localhost:4999/paperlib/users/' + id)
-      .map(response => response.json());
+      .pipe(map(response => response.json()));
   }
 }
